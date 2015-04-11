ab7d490523d1b4ee9f63e34a92e0c1c04d04408d
Marin Atanasov <tyxla@abv.bg>
2015-04-11 22:17:40 +0300

    admin form field - text

---
+++ includes/field-text.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Manages and renders a text settings field.
+ *
+ * @uses QBPPC_Field
+ */
+class QBPPC_Field_Text extends QBPPC_Field {
+
+	/**
+	 * Render this field.
+	 *
+	 * @access public
+	 */
+	public function render() {
+		?>
+		<input name="<?php echo $this->get_id(); ?>" id="<?php echo $this->get_id(); ?>" type="text" value="<?php echo esc_attr($this->get_value()); ?>" class="regular-text" />
+		<?php
+		$this->render_help();
+	}
+
+}
