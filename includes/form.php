29e555530e937b863fa255bd9bd7c88e22e55da3
Marin Atanasov <tyxla@abv.bg>
2015-04-11 22:40:43 +0300

    form - register fields

--- includes/form.php
+++ includes/form.php
@@ -13,7 +13,10 @@ class QBPPC_Form {
 	 */
 	public function __construct() {
 		// hook the main plugin page
-		add_action('admin_menu', array($this, 'add_submenu_page'));
+		add_action( 'admin_menu', array( $this, 'add_submenu_page' ) );
+
+		// register settings fields & sections
+		add_action( 'admin_init', array( $this, 'register_settings' ) );
 	}
 
 	/**
@@ -51,7 +54,71 @@ class QBPPC_Form {
 		$menu_id = $this->get_menu_id();
 
 		// register the submenu page - child of the Settings parent menu item
-		add_submenu_page( 'options-general.php', $menu_title, $menu_title, 'publish_posts', $menu_id, array($this, 'render') );
+		add_submenu_page(
+			'options-general.php',
+			$menu_title,
+			$menu_title,
+			'publish_posts',
+			$menu_id,
+			array($this, 'render')
+		);
+
+		// register settings section
+		add_settings_section(
+			$menu_id,
+			'',
+			'',
+			$menu_id
+		);
+
+	}
+
+	/**
+	 * Get field data. Defines and describes the fields that will be registered.
+	 *
+	 * @access public
+	 *
+	 * @return array $fields The fields and their data.
+	 */
+	public function get_field_data() {
+		return array(
+			'hierarchy_indent_character' => array(
+				'type' => 'text',
+				'title' => 'Hierarchy Indent Character',
+				'default' => '*',
+				'help' => 'You can use this character at the beginning of your entry to specify hierarchy indentation.',
+			),
+			'post_type' => array(
+				'type' => 'select',
+				'title' => 'Post Type',
+				'default' => '',
+				'help' => 'The post type that you want to bulk insert entries into.',
+				'options' => QBPPC_Posts::get_post_types(),
+			),
+			'pages' => array(
+				'type' => 'textarea',
+				'title' => 'Entries (one per line)',
+				'default' => '',
+				'help' => 'A hierarchical list of your entries.',
+			),
+		);
+	}
+
+	/**
+	 * Register the settings sections and fields.
+	 *
+	 * @access public
+	 */
+	public function register_settings() {
+		// register fields
+		$field_data = $this->get_field_data();
+		foreach ($field_data as $field_id => $field) {
+			$field_object = QBPPC_Field::factory($field['type'], 'qbppc_' . $field_id, $field['title'], $this->get_menu_id(), $this->get_menu_id());
+			if (isset($field['options'])) {
+				$field_object->set_options($field['options']);
+			}
+			$this->fields[] = $field_object;
+		}
 	}
 
 	/**
