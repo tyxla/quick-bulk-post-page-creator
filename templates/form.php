db78ef175d59c1b5ee8690ec27ee23c3c1d237cb
Marin Atanasov <tyxla@abv.bg>
2015-04-11 22:39:43 +0300

    form template - add form, fields & button

--- templates/form.php
+++ templates/form.php
@@ -3,5 +3,14 @@
 		<h2><?php echo $qbppc->get_form()->get_menu_title(); ?></h2>
 
 		<p><?php _e('Please, select a post type and input your preferred hierarchy.', 'qbppc'); ?></p>
+
+		<form method="post" action="">
+			<?php 
+			settings_fields( $qbppc->get_form()->get_menu_id() );
+			do_settings_sections( $qbppc->get_form()->get_menu_id() );
+			submit_button(__('Bulk Insert', 'qbppc')); 
+			?>
+		</form>
+
 	</div>
 </div>
