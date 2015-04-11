3f17cb6f45a8d5acac7d8d37a700bae616525aa3
Marin Atanasov <tyxla@abv.bg>
2015-04-11 22:17:51 +0300

    admin form field - textarea

---
+++ includes/field-textarea.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Manages and renders a textarea settings field.
+ *
+ * @uses QBPPC_Field
+ */
+class QBPPC_Field_Textarea extends QBPPC_Field {
+
+	/**
+	 * Render this field.
+	 *
+	 * @access public
+	 */
+	public function render() {
+		?>
+		<textarea name="<?php echo $this->get_id(); ?>" id="<?php echo $this->get_id(); ?>" class="large-text" rows="20"><?php echo esc_html($this->get_value()); ?></textarea>
+		<?php
+		$this->render_help();
+	}
+
+}
